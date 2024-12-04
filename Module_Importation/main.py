b9ae2a717d42caa5b790703b5d85cd513917ba47
PaulErnest <PaulErMartin@yahoo.fr>
2024-12-04 16:07:44 +0200

    import exmaple

---
+++ Module_Importation/main.py
@@ -0,0 +1,8 @@
+print("Bonjour la famille")
+
+import module
+from module import nom_de_famille
+
+print(nom_de_famille)
+
+module.salut("MARTIN ET DUPONT")
---
+++ Module_Importation/module.py
@@ -0,0 +1,7 @@
+nom_de_famille = "MARTIN"
+
+def salut(nom):
+    print(f"Bonjour {nom}")
+
+if __name__ == "__main__":
+    salut("TEST")
